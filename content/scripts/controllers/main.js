ed6bc6cca9374d49630576f7de7e8f1c2f1c92c2
Alejandro Figueroa <alexphi@gmail.com>
2017-02-12 10:13:11 -0500

    Added controllers and views

---
+++ content/scripts/controllers/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name markdweet-app.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the markdweet-app
+ */
+angular.module('markdweet-app')
+  .controller('main-controller', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS'
+    ];
+  });
--- content/scripts/main.js
+++ content/scripts/main.js
@@ -1,1 +1,33 @@
+'use strict';
+
 console.log('\'Allo \'Allo!');
+
+/**
+ * @ngdoc overview
+ * @name markdweet-app
+ * @description
+ * # markdweet-app
+ *
+ * Main module of the application.
+ */
+angular
+  .module('markdweet-app', [
+    'ngRoute'
+  ])
+  .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    $locationProvider.hashPrefix('')
+
+    $routeProvider
+      .when('/', {
+        templateUrl: 'content/views/main.html',
+        controller: 'main-controller'
+      })
+      .when('/about', {
+        templateUrl: 'content/views/about.html',
+      })
+      .otherwise({
+        redirectTo: '/'
+      })
+  }])
+
+
